c693ffe9de88b2016c9c39e376d7c7280ac16711
tw168 <tony.wei@outlook.com>
2023-07-28 15:46:20 -0500

    working on trcuk log

--- pages/2_Truck log.py
+++ pages/2_Truck log.py
@@ -1,19 +1,24 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 # from sqlalchemy import create_engine
 
 # Connect to the database
 conn = st.experimental_connection('ws_hub', type='sql')
-
 conn
-# Function to insert data into the table
-def insert_data(data):
-    conn.execute('''
-            INSERT INTO truck_log (schd_date, aal, cal, charger, chr, drake, geodis, giltner, hellman, ip, jbht, landair, neon, pdi, qtbk, traf, saia, xpo)
-            VALUES (:schd_date, :aal, :cal, :charger, :chr, :drake, :geodis, :giltner, :hellman, :ip, :jbht, :landair, :neon, :pdi, :qtbk, :traf, :saia, :xpo)
-        ''', data)
 
+with st.expander("Expander", expanded=True):
+    date_col, col2, col3, col4, col5 = st.columns(5)
+    with date_col:
+        sche_date = st.date_input("Date", datetime.now())
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    with col1:
+        aal = st.number_input("AAL", min_value=0, max_value=10)
+        cal = st.number_input("CAL", min_value=0, max_value=10)
+        chr = st.number_input("CHR", min_value=0, max_value=10)
+    with col2:
+        darke = st.number_input("DRAKE", min_value=0, max_value=10)
+        giltner = st.number_input("Giltner", min_value=0, max_value=10)
+        geodis = st.number_input("GEODIS", min_value=0, max_value=10)
 
-
-            
-
+    
